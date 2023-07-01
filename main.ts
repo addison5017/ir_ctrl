8e3b6da9f6e3bca5be3bceff433effa0a3cb3b21
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 15:11:45 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,39 @@
+let IR = 0
+BitIR.IR_callbackUserV2(function (message) {
+    if (message == BitIR.IR_KeyValue(BitIR.enIRButton.Power)) {
+        IR = 1
+    } else if (message == BitIR.IR_KeyValue(BitIR.enIRButton.Light)) {
+        IR = 2
+    } else if (message == BitIR.IR_KeyValue(BitIR.enIRButton.One)) {
+        IR = 3
+    } else if (message == BitIR.IR_KeyValue(BitIR.enIRButton.Two)) {
+        IR = 4
+    }
+})
+basic.forever(function () {
+    if (IR == 1) {
+        IR = 0
+        mbit_小車類.RGB_Car_Big2(mbit_小車類.enColor.OFF)
+    } else if (IR == 2) {
+        IR = 0
+        mbit_小車類.RGB_Car_Big2(mbit_小車類.enColor.Red)
+    } else if (IR == 3) {
+        IR = 0
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (IR == 4) {
+        IR = 0
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+    }
+})
